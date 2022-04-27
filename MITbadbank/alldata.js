4f450bdc6ca2756df1535d9a5be838e06a0dc6ae
Johnny Johnson <89882915+MrJohnnyJohnson@users.noreply.github.com>
2022-04-27 08:42:35 -0700

    alldata actual
    icloud had not updated the local directory prior to push. Here is the commit with alldata styling and user table.

--- MITbadbank/alldata.js
+++ MITbadbank/alldata.js
@@ -1,10 +1,32 @@
 function AllData(){
-    const ctx = React.useContext(UserContext);
-    return (
-      <>
-      <h5>Bank Activity History</h5>
-      {JSON.stringify(ctx)}<br/>
-      </>
-    );
-  }
-  
+  const ctx = React.useContext(UserContext);
+  return (
+    <>
+   <h1>All User Banking Data</h1>
+    {/* {JSON.stringify(ctx)}<br/> */}
+      
+    <table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">User</th>
+      <th scope="col">Email</th>
+      <th scope="col">Password</th>
+      <th scope="col">Balance</th>
+    </tr>
+  </thead>
+  <tbody>
+        {ctx.users.map((user) => {
+                    return (
+          <tr>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.password}</td>
+            <td>{user.balance}</td>
+          </tr>
+                    )
+        })}
+        </tbody>
+</table>
+    </>
+  );
+}
